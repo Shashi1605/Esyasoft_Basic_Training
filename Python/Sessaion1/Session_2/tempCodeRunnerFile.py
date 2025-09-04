d4badcfd9c29c3c72dff815f7a26450d31df038a
Shashi Kumar <Shashi.ranjan@esyasoft.com>
2025-09-04 14:05:44 +0530

    Assisgment_Day_1

---
+++ Python/Sessaion1/Session_2/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+for i in fruits:
+    if i == 4:
+        break
+    elif i == "Banana":
+        continue
+    else:
+        print(i)
--- Python/Sessaion1/main.py
+++ Python/Sessaion1/main.py
@@ -1,1 +1,8 @@
-print("Hello World!")  
+
+#checking type of variable
+# a = 9
+# print(type(a))
+
+#calculating area of square
+x = int(input("Enter the Length of Side : "))
+print("Square of ", x , " : ", x*x)
